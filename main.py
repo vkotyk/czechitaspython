dfdd694e141f90a726083851c162bd2dd9f9dfe5
Václav Kotyk <vaclav_kotyk@kb.cz>
2020-05-17 09:05:11 +0000

    více variant zápisu

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #Vytvoř (nadefinuj) dvě proměnné, jmeno a vek. Následně do nich přiřaď hodnotu. Nakonec obsah proměnných vypiš na obrazovku.
-jmeno = "Jan Novák"
-vek = 20
-print("Jmenuji se", jmeno, "a je mi", vek, "let")
+jmeno = "Václav Kotyk"
+vek = 25
+email = "vaclavkotyk@seznam.cz"
+print("Jmenuji se", jmeno, "a je mi", vek, "let a kontakt na mě je", email)
+
 #Vytvoř dvě číselné proměnné a pak jednu další, ve které bude součet hodnot z předchozích dvou proměnných. Výsledek vypiš na obrazovku.
 cisilko1 = 10
 cisilko2 = 8
 soucet = cisilko1 + cisilko2
 print(soucet)
+
 #Vytvoř proměnné cislo1 a cislo2. Do konzole vypiš jejich součet, součin, rozdíl a podíl, ve formátu "1 + 1 = 2".
 cislo1 = 24
 cislo2 = 8
@@ -14,7 +17,10 @@ print(cislo1, "+", cislo2, "=", (cislo1+cislo2))
 print(cislo1, "-", cislo2, "=", (cislo1-cislo2))
 print(cislo1, "*", cislo2, "=", (cislo1*cislo2))
 print(cislo1, "/", cislo2, "=", (cislo1/cislo2))
+
 #Vytvoř proměnnou obsahující text tvého křestního jména (tak aby nezačínal a nekončil mezerou). Vytvoř druhou proměnnou obsahující text tvého příjmení (opět nesmí začínat ani končit mezerou). Nakonec vytvoř proměnnou, ve které bude "spojení" těchto dvou proměnných s tím, že je spojíš mezerou. Výsledný text vypiš na obrazovku.
 jmeno = "Václav"
 prijmeni = "Kotyk"
+print(jmeno+" "+prijmeni)
+print(jmeno+""+prijmeni)
 print(jmeno,prijmeni)
