70c3bbc4c3c7fe446e1b69f23539626b7cd4f094
Václav Kotyk <vaclav_kotyk@kb.cz>
2020-05-17 08:39:21 +0000

    další ukoly

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+#Vytvoř (nadefinuj) dvě proměnné, jmeno a vek. Následně do nich přiřaď hodnotu. Nakonec obsah proměnných vypiš na obrazovku.
 jmeno = "Jan Novák"
 vek = 20
 print("Jmenuji se", jmeno, "a je mi", vek, "let")
+#Vytvoř dvě číselné proměnné a pak jednu další, ve které bude součet hodnot z předchozích dvou proměnných. Výsledek vypiš na obrazovku.
+int = 10
+int = 8
+print(int, "+", int)
+#Vytvoř proměnné cislo1 a cislo2. Do konzole vypiš jejich součet, součin, rozdíl a podíl, ve formátu "1 + 1 = 2".
+cislo1 = 24
+cislo2 = 8
+print(cislo1, "+", cislo2, "=", (cislo1+cislo2))
+print(cislo1, "-", cislo2, "=", (cislo1-cislo2))
+print(cislo1, "*", cislo2, "=", (cislo1*cislo2))
+print(cislo1, "/", cislo2, "=", (cislo1/cislo2))
+#Vytvoř proměnnou obsahující text tvého křestního jména (tak aby nezačínal a nekončil mezerou). Vytvoř druhou proměnnou obsahující text tvého příjmení (opět nesmí začínat ani končit mezerou). Nakonec vytvoř proměnnou, ve které bude "spojení" těchto dvou proměnných s tím, že je spojíš mezerou. Výsledný text vypiš na obrazovku.
+jmeno = "Václav"
+prijmeni = "Kotyk"
+print(jmeno,prijmeni)
